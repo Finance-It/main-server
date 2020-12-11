303eb208638b7957e663a420e3ce073b9fd05820
AVC0706 <atharvvijay4@gmail.com>
2020-12-12 00:51:28 +0530

    Celery Function

--- app/payments/tasks.py
+++ app/payments/tasks.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+
+import pytz
 from celery.schedules import crontab
 from celery.task import periodic_task
 
+from campaign.models import Campaign
+
 
 @periodic_task(run_every=crontab(minute=0, hour=0))
-def periodic_check_campaign():
-    print('lol')
+def campaign_target_check():
+    curr_time = datetime.now(tz=pytz.UTC)
+    campaign = Campaign.objects.filter(
+        end_date__lte=curr_time, )
+
+    return
