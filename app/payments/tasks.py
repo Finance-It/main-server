51bb3155450144c8907df8671c2deb17433ab78b
pratikdaigavane <daigavanep@gmail.com>
2020-12-12 00:35:53 +0530

    bug fixes

--- app/payments/tasks.py
+++ app/payments/tasks.py
@@ -2,6 +2,6 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 
 
-@periodic_task(run_every=crontab(minute=0, hour='0'))
+@periodic_task(run_every=crontab(minute=0, hour=0))
 def periodic_check_campaign():
     print('lol')
